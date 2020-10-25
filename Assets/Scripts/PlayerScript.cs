cc3b5f81cd4529ccbcfbc23eb9d049f2ecc9a4e4
Callie Figueroa <calliefigueroa@knights.ucf.edu>
2020-10-25 18:25:35 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +9,48 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
 
-    public float speed;
-
     public Text score;
+    public Text lives;
+    public Text winText;
+    public Text loseText;
+    public Text controlText;
 
     private int scoreValue = 0;
+    private int livesValue = 3;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public float speed;
+
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        controlText.text = "Controls: WASD";
+        SetScore();
+        SetLives();
+
+        winText.text =  "";
+        loseText.text = "";
+
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +59,15 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,19 +75,81 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScore();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 5)
+            {
+                transform.position = new Vector2(24.0f, -1.0f);
+                livesValue = 3;
+                SetLives();
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            SetLives();
+            Destroy(collision.collider.gameObject);
+        }
+  
+
+        if (scoreValue == 9)
+        {
+            winText.text = "You win! - Callie Figueroa";
+
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+        if (livesValue == 0)
+        {
+            loseText.text = "You lose!";
+            Destroy(this);
+            musicSource.Stop();
         }
 
     }
 
+    void SetScore()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        if (scoreValue >= 9 && winText != null)
+        {
+            winText.text = "You win! - Callie Figueroa";
+            Destroy(this);
+            musicSource.Stop();
+        }
+    }
+    void SetLives()
+    {
+        lives.text = "Lives: " + livesValue.ToString();
+        if (livesValue <= 0)
+        {
+            loseText.text = "You lose!";
+            Destroy(this);
+            musicSource.Stop();
+        }
+    } 
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         if (collision.collider.tag == "Ground")
         {
             if (Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                anim.SetTrigger("takeOff");
+                
             }
         }
     }
@@ -55,5 +159,27 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (Input.GetKeyDown(KeyCode.D))
+
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+
+        {
+            anim.SetInteger("State", 0);
+        }
+
     }
 }
